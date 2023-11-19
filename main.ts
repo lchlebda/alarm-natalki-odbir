22e3208ab99cf956d13c49676bd90cf402e07199
aitseb <lchlebda@gmail.com>
2023-11-19 18:52:43 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "alarm") {
-        music.play(music.tonePlayable(988, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
-        basic.pause(500)
+        for (let index = 0; index < 4; index++) {
+            music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+            basic.pause(200)
+        }
     }
 })
 radio.setGroup(255)
